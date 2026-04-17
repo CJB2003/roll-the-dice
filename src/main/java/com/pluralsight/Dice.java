14f93ba73442de3106c82767adb8f9067cae9d24
chrisbunny <christopherjbunny@gmail.com>
2026-04-17 13:52:13 -0500

    Finished displaying counters and the if statements needed for each counter value.

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class Dice {
+
+    //roll method that returns a random number between 6 and 1
+    public int roll() {
+        int randomNumber = (int)(Math.random() * 6) + 1;
+        return randomNumber;
+    }
 }
